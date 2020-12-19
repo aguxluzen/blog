edb1a052591bed6bf86bd7306830225883f09238
Agustin Luzenti <aguss.luzenti@gmail.com>
2020-12-18 21:28:46 -0300

    Estados en react

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function Alaskdj(props) {
+  return (
+    <>
+      <div>{props.asd}</div>
+    </>
+  );
+}
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function aumentarContador() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={aumentarContador}>CONTAR</button>
+      <Alaskdj asd={counter}></Alaskdj>
     </div>
   );
 }
