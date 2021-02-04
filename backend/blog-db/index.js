d37be9f5a691a9d2f26a3af48e5b7c9f683c4fac
Agustin Luzenti <aguss.luzenti@gmail.com>
2021-02-03 21:21:32 -0300

    agrego comentario

--- backend/blog-db/index.js
+++ backend/blog-db/index.js
@@ -8,6 +8,7 @@ module.exports = async function (config) {
 
   await sequelize.authenticate();
 
+  //Si setup esta en false no limpia la base de datos ni recrea tablas
   if (config.setup) {
     await sequelize.sync({ force: true });
   }
